8978060a333317ef0c2a5276c95ade8e38a32d2e
S. Paquette <spaquett@systemsbiology.org>
2026-04-20 16:36:20 -0700

    -> Use update-alternatives to ensure the right version of python is used -> Make sure to add /home/circleci/.pyenv/shims to the path (it's not there for sudo)

--- shell/database-setup.sh
+++ shell/database-setup.sh
@@ -1,10 +1,8 @@
 if [ -n "$CI" ]; then
-    # Force CircleCI's .pyenv back into the path
-    export PATH=/home/circleci/.pyenv/shims:/usr/local/bin:$PATH
-    echo $PATH
-    sudo update-alternatives --install /usr/local/bin/python3 python /home/circleci/.pyenv/shims/python3 6
+    # Force CircleCI's .pyenv back into the path and use update alternatives to require python 3.11
+    export PATH=/home/circleci/.pyenv/shims:$PATH
+    update-alternatives --install /usr/local/bin/python3 python /home/circleci/.pyenv/shims/python3 6
     python3 --version
-    python --version
     export HOME=/home/circleci/${CIRCLE_PROJECT_REPONAME}
     export HOMEROOT=/home/circleci/${CIRCLE_PROJECT_REPONAME}
     # Set test database settings; this database will be thrown away at the end
--- shell/install-deps.sh
+++ shell/install-deps.sh
@@ -1,12 +1,9 @@
 if [ -n "$CI" ]; then
-    # Force CircleCI's .pyenv back into the path
+    # Force CircleCI's .pyenv back into the path and use update alternatives to require python 3.11
     export PATH=/home/circleci/.pyenv/shims:$PATH
-    ls -l /usr/local/bin
-    ls -l /usr/local/sbin
     update-alternatives --install /usr/local/bin/python3 python /home/circleci/.pyenv/shims/python3 6
     echo "[STATUS] Check our Python and OS versions:"
     python3 --version
-    which python3
 
     echo "OS:"
     cat /etc/os-release
