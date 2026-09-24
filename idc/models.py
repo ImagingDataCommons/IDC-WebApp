bbbc459eb38ce45ead0cf9d9202923a57b7fbebe
S. Paquette <spaquett@systemsbiology.org>
2026-09-23 17:24:39 -0700

    -> SharedCart fixes

--- idc/models.py
+++ idc/models.py
@@ -48,6 +48,7 @@ class SharedCart(models.Model):
     SERIES_IDS_MAX = 64000
     CART_MAX_PER_IP = 100
     CART_PER_MIN_MAX = 2
+    DEFAULT_SOURCE_IP='10.0.0.2'
     cart_id = models.CharField(primary_key=True, max_length=255, null=False, blank=False, default=cart_keygen)
     source_ip = models.GenericIPAddressField(null=False, blank=False, default='0.0.0.0')
     created_on = models.DateTimeField(auto_now_add=True, null=False, blank=False)
